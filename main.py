5d031249a30c01a0e4babfc8ba079b413a39318b
go39xul <go39xul@mytum.de>
2025-02-26 13:33:16 +0100

    feat/ rotation/corners update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import csv
 import time
 import logging
+import json
 
 import pandas as pd
 
@@ -31,98 +32,22 @@ def main() -> None:
     ## Load the arguments ##
     args = Arguments.parse_arguments()
     model = args.model
-    ground_truth_data = args.ground_truth_data
     sources_traductor = args.sources_traductor
-    # output_csv = args.output_csv
-
-    # output = pd.read_csv(output_csv)
-    # if output.empty:
-    #     output = pd.DataFrame(columns=["image", "candidate", "json"])
-
-
-
-    gt_data = pd.read_csv(ground_truth_data)
-    prefix = "/home/guillfa/CENIA/sources-traductor/"
-    gt_data['image'] = gt_data['image'].apply(lambda x: prefix + x)    
-    images_folder = gt_data['image'].tolist()
 
     sources_traductor = get_list(sources_traductor)
-    sources_traductor = [img for img in sources_traductor if os.path.basename(img) not in {os.path.basename(c) for c in images_folder}]
-    random.shuffle(sources_traductor)
-    sources_traductor = sources_traductor[:300]
 
-    # images = images_folder + sources_traductor
-    images = images_folder
     ## Load the database ##
     database = Mongo(connection_uri=os.getenv("MONGO_URI"))
 
     # ## Load the model ##
     logger.info("Initializing model...")
-    if model == "sonnet":
-        model = ClaudeAPI(model_name="claude-3-5-sonnet-20241022", prompt_type="Classifier", tool="extract_json")
-        cost = model.calculate_cost()
-        logger.info(f"Initial cost: {cost}")
 
-    processed_images = {
-        "pairs_candidates": {},
-        "sources_traductor": {}
-    }
-
-
-
-    for idx, file_path in enumerate(tqdm(images, desc="Processing sources-traductor images")):
+    for idx, file_path in enumerate(tqdm(sources_traductor, desc="Processing sources-traductor images")):
         print("#"*50)
-        if idx >= 50:
-            break
         image_name = os.path.basename(file_path)
         logger.debug(f"Processing image: {image_name}")
-        # if image_name in output['image'].values:
-        #     logger.info(f"Image '{image_name}' already processed. Skipping...")
-        #     continue
-
         image = Image_(path=file_path)
-        image.resize_aspect_ratio()
 
-        class_answer = model.run_inference(image, max_retries=3, timeout=15)
-        cost = model.calculate_cost()
-        logger.info(f"Current cost: {cost}")
 
-        if class_answer != "Candidate":
-            logger.info(f"Image '{image_name}' is not a candidate. Continuing...")
-            continue
-
-        model.set_prompt("JSON_extractor")
-        answer = model.run_inference(image, max_retries=3, timeout=15)
-        model.set_prompt("Classifier")
-        cost = model.calculate_cost()
-        logger.info(f"Current cost: {cost}")
-        
-        if model.total_cost > 30:
-            logger.warning("Cost limit reached. Exiting.")
-            break
-
-        processed_images["pairs_candidates"][file_path.split('/')[-1]] = {
-            "candidate": class_answer,
-            "json": answer
-        }
-        logger.info(f"Saving results for '{image_name}' to CSV.")
-        print(f"pairs_candidates: {file_path.split('/')[-1]}")
-        print("candidate: ", class_answer)
-        print("json: ", answer)
-
-    
-        # save_to_csv(
-        #     output_csv, {"ground_truth": {file_path.split('/')[-1]: 
-        #                                        {"candidate": class_answer,
-        #                                         "json": answer
-        #                                         }
-        #                                     }
-        #                 }
-        #             )    
-    # logger.info("Calculating metrics...")    
-    # Calculate metrics (Can be improved)
-    # calculate_metrics(output_csv)
-
-    
 if __name__ == "__main__":
     main()
--- utils/mongo.py
+++ utils/mongo.py
@@ -1,12 +1,16 @@
 import pymongo
 import logging
-from typing import Optional, Dict, Any
+import json
+from typing import Optional, Dict, Any, List
+from pymongo import UpdateOne, MongoClient
+from bson.objectid import ObjectId
 
 logger = logging.getLogger(__name__)
 
 class Mongo:
     def __init__(self, connection_uri: str):
         self.client = pymongo.MongoClient(connection_uri)
+        self.collection = self.client.data_repository.files
         self._test_connection()
 
     def extract_metadata(self, image_path: str) -> Optional[Dict[str, Any]]:
@@ -25,6 +29,69 @@ class Mongo:
 
         return {"rotation": rotation, "corners": corners}
 
+    def update_rotation_bulk(self, updates: List[Dict[str, Any]]) -> None:
+        if not updates:
+            logger.warning("No updates to perform.")
+            return
+        
+        bulk_updates = [
+            self._update_rotation_query(
+                update["current_mongo_element"],
+                update["new_rotation"],
+                update["technique_used"],
+                update["confidence"],
+                update["is_new_best"]
+            ) for update in updates
+        ]
+
+        if bulk_updates:
+            try:
+                result = self.collection.bulk_write(bulk_updates, ordered=False)
+                logger.info("Bulk update completed: Matched: %d, Modified: %d", result.matched_count, result.modified_count)
+            except pymongo.errors.PyMongoError as e:
+                logger.exception("Bulk update failed: %s", e)
+                raise
+
+    def update_corners_bulk(self, json_path: str) -> None:
+        try:
+            with open(json_path) as f:
+                inferences_data = json.load(f)
+        except Exception as e:
+            logger.error("Failed to read JSON file: %s", e)
+            raise
+
+        bulk_updates = []
+
+        for item in inferences_data:
+            filename = item.get("filename")
+            corners = item.get("corners", [])
+
+            if not filename:
+                continue
+
+            document = self.collection.find_one({"blob_filename": filename})
+
+            if document:
+                bulk_updates.append(
+                    self._update_corners_query(
+                        document,
+                        corners,
+                        "SegFormerb5 versión febrero 2025",
+                        "v1",  # Hardcoded for now, can be improved later
+                        True  # is_new_best
+                    )
+                )
+
+        if bulk_updates:
+            try:
+                result = self.collection.bulk_write(bulk_updates, ordered=False)
+                logger.info("Bulk update completed: Matched: %d, Modified: %d", result.matched_count, result.modified_count)
+            except pymongo.errors.PyMongoError as e:
+                logger.exception("Bulk update failed: %s", e)
+                raise
+        else:
+            logger.info("No updates to process.")
+
     def _test_connection(self) -> None:
         try:
             self.client.admin.command('ping')
@@ -32,3 +99,68 @@ class Mongo:
         except Exception as e:
             logger.exception("Connection to MongoDB failed: %s", e)
             raise
+
+    def _get_next_key(self, mongo_element: Dict[str, Any]) -> str:
+        if 'detections_history' not in mongo_element:
+            return 'v0'
+        existing_keys = [int(s.replace('v', '')) for s in mongo_element['detections_history'].keys()]
+        next_key = f'v{max(existing_keys) + 1}'
+        return next_key
+
+    def _update_rotation_query(
+        self,
+        current_mongo_element: Dict[str, Any],
+        new_rotation: float,
+        technique_used: str,
+        confidence: float,
+        is_new_best: bool
+    ) -> UpdateOne:
+
+        update_query = {}
+        element_id = current_mongo_element['_id']
+        next_key = self._get_next_key(current_mongo_element)
+
+        if 'rotation' not in current_mongo_element.get('best_metadata', {}):
+            is_new_best = True
+
+        if is_new_best:
+            update_query["best_metadata.rotation"] = next_key
+
+        new_entry = {
+            'rotation': new_rotation,
+            'source': 'root_file',
+            'technique_used': technique_used,
+            'confidence': confidence,
+            'usable_for_training': False
+        }
+        update_query[f"detections_history.{next_key}"] = new_entry
+
+        return UpdateOne({"_id": ObjectId(element_id)}, {"$set": update_query})
+
+    def _update_corners_query(
+        self,
+        current_mongo_element: Dict[str, Any],
+        new_corners: List[float],
+        technique_used: str,
+        rotation_used: str,
+        is_new_best: bool
+    ) -> UpdateOne:
+        update_query = {}
+        element_id = current_mongo_element['_id']
+        next_key = self._get_next_key(current_mongo_element)
+
+        if 'corners' not in current_mongo_element.get('best_metadata', {}):
+            is_new_best = True
+
+        if is_new_best:
+            update_query["best_metadata.corners"] = next_key
+
+        new_entry = {
+            'corners': new_corners,
+            'source': f'root_file + rotate_{rotation_used}',
+            'technique_used': technique_used,
+            'usable_for_training': False
+        }
+        update_query[f"detections_history.{next_key}"] = new_entry
+
+        return UpdateOne({"_id": ObjectId(element_id)}, {"$set": update_query})
