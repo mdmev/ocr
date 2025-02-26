0650fee593766151dbe7934a0a501bd04d0cf9c3
go39xul <go39xul@mytum.de>
2025-02-26 13:47:34 +0100

    feat/ added prediction fc for the resnet

--- models/resnet_rot_detector.py
+++ models/resnet_rot_detector.py
@@ -1,3 +1,6 @@
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
 import torch.nn as nn
 import torchvision
 
@@ -14,6 +17,24 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         return self.resnet50(x)
+
+def predict(model, dataloader, device):
+    results = []
+    processed_names = set()  # Usar un conjunto para manejar nombres únicos procesados
+    with torch.no_grad():
+        for images, names in tqdm(dataloader,desc="Processing images"):
+            images = images.to(device)
+            outputs = model(images)
+            probabilities = F.softmax(outputs, dim=1)
+            confidences, predictions = torch.max(probabilities, dim=1)
+            for name, prediction, confidence in zip(names, predictions.cpu().numpy(), confidences.cpu().numpy()):
+                
+                if name not in processed_names:  # Solo añadir si no es duplicado
+                    processed_names.add(name)
+                    results.append((name, prediction, confidence))
+                else:
+                    print(f"Duplicate image name found and skipped: {name}")
+    return results
 
 
 if __name__ == "__main__":
